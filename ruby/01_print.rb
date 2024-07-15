e755fbdb36279551d0f50f17dd95b28ca107f151
Rohit Kushwaha <rajdilkhush0987654321@gmail.com>
2024-07-16 00:00:33 +0530

    day one in ruby

---
+++ ruby/01_print.rb
@@ -0,0 +1,4 @@
+# print "Hello World!" # prints any object
+puts "Hello World!" # same as print but adds a newline
+
+print "\n\n\n" # prints a newline
---
+++ ruby/04_interplation.rb
@@ -0,0 +1,11 @@
+
+name = "John"
+
+puts "Hello #{name}!" # interpolation
+# puts "Hello #{name.upcase}!" # interpolation
+# puts "Hello #{name.downcase}!" # interpolation
+# puts "Hello #{name.capitalize}!" # interpolation
+
+
+
+puts 'Hello #{name}!' # Not Working
